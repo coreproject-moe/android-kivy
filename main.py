364f511ac7a3da0ec09c6e0d12df06d12904dab3
tokitou-san <suneeth.since2005@gmail.com>
2023-07-23 21:44:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 from kivymd.app import MDApp
 from kivymd.uix.label import MDLabel
 
-class App(MDApp):
-	def build(self):
-		return MDLabel(
-			text="CoreProject",
-			halign="center"
-		)
 
-if __name__ == "__main__":
-	App().run()
+class MainApp(MDApp):
+    def build(self):
+        return MDLabel(text="CoreProject", halign="center")
+
+
+MainApp().run()
