5efd9f89180aa9a88826d54d609ff793e584db0b
tokitou-san <tokitousan2005@gmail.com>
2023-07-26 03:10:00 +0530

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,18 @@
+from kivy import Config
+# config window size
+Config.set("graphics", "width", 360)
+Config.set("graphics", "height", 640)
+
+from libs.screens.home_page import HomePage
+from kivymd.tools.hotreload.app import MDApp
+
+class MainApp(MDApp):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def build_app(self):
+        return HomePage()
+
+if __name__ == "__main__":
+    MainApp().run()
