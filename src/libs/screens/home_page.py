e52a196726dd6a6810020af061bbc9b0d97067f6
Tokito <tokitousan2005@gmail.com>
2023-07-26 04:46:35 +0530

    refactor: new elements aligning layout (#13)
    * Update home_page.py
    
    * Update home_page.py

--- src/libs/screens/home_page.py
+++ src/libs/screens/home_page.py
@@ -1,3 +1,5 @@
+from kivy.uix.widget import Widget
+from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.screen import MDScreen
 from kivymd.uix.relativelayout import MDRelativeLayout
 from kivymd.uix.button import MDFillRoundFlatButton
@@ -11,16 +13,49 @@ class HomePage(MDScreen):
         self.md_bg_color = get_color_from_hex("#03020c")
 
     def layout(self):
-        app_bar_layout = MDRelativeLayout(
+        # global variables
+        empty_space = Widget()
+
+        # Appbar elements
+        app_bar_layout = MDBoxLayout(
             size_hint_y = None,
             height = "50dp",
-            pos_hint = {"top": 1}
+            pos_hint = {"top": 1},
+            padding = "10dp"
         )
-
-        button = MDFillRoundFlatButton(
+        # CoreProject logo
+        core_logo = MDFillRoundFlatButton(
             text="Logo",
             pos_hint={"x": 0, "top": 1}
         )
-        app_bar_layout.add_widget(button)
+        # Right side elements
+        appbar_right = MDBoxLayout(
+            orientation = "horizontal",
+            spacing = "5dp"
+        )
+        # Search button
+        search_button = MDFillRoundFlatButton(
+            text="Search",
+            pos_hint={"x": 0, "top": 1}
+        )
+        # Notification button
+        notification_button = MDFillRoundFlatButton(
+            text="Noti",
+            pos_hint={"x": 0, "top": 1}
+        )
+        # Profile button
+        profile_button = MDFillRoundFlatButton(
+            text="Tokito",
+            pos_hint={"x": 0, "top": 1}
+        )
+        # Add an empty Widget for aligning items left and right
+        appbar_right.add_widget(empty_space)
+        # Add other widgets
+        appbar_right.add_widget(search_button)
+        appbar_right.add_widget(notification_button)
+        appbar_right.add_widget(profile_button)
+        # Add wigets to main Appbar layout
+        app_bar_layout.add_widget(core_logo)
+        app_bar_layout.add_widget(appbar_right)
 
         return app_bar_layout
