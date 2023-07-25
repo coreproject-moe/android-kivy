8e1334deb849ab8c0acddd9226f02c0459448040
tokitou-san <tokitousan2005@gmail.com>
2023-07-26 04:08:57 +0530

    Update home_page.py

--- src/libs/screens/home_page.py
+++ src/libs/screens/home_page.py
@@ -1,12 +1,14 @@
 from kivymd.uix.screen import MDScreen
 from kivymd.uix.relativelayout import MDRelativeLayout
 from kivymd.uix.button import MDFillRoundFlatButton
+from kivy.utils import get_color_from_hex
 
 class HomePage(MDScreen):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         self.add_widget(self.layout())
+        self.md_bg_color = get_color_from_hex("#03020c")
 
     def layout(self):
         app_bar_layout = MDRelativeLayout(
