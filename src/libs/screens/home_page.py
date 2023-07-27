84ab743d299d37794d91f28b930b8e543092ecc6
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-07-27 22:17:07 +0600

    Add Pre Commit (#22)
    * add
    
    * Fix Buildozer
    
    Update buildozer.spec
    
    Update buildozer.spec
    
    Update buildozer.yml
    
    Update buildozer.yml
    
    * Create .pre-commit-config.yaml
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update .pre-commit-config.yaml
    
    * fix
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/libs/screens/home_page.py
+++ src/libs/screens/home_page.py
@@ -2,6 +2,7 @@ from kivymd.uix.screen import MDScreen
 from kivymd.uix.relativelayout import MDRelativeLayout
 from kivymd.uix.button import MDFillRoundFlatButton
 
+
 class HomePage(MDScreen):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -10,15 +11,10 @@ class HomePage(MDScreen):
 
     def layout(self):
         app_bar_layout = MDRelativeLayout(
-            size_hint_y = None,
-            height = "50dp",
-            pos_hint = {"top": 1}
+            size_hint_y=None, height="50dp", pos_hint={"top": 1}
         )
 
-        button = MDFillRoundFlatButton(
-            text="Logo",
-            pos_hint={"x": 0, "top": 1}
-        )
+        button = MDFillRoundFlatButton(text="Logo", pos_hint={"x": 0, "top": 1})
         app_bar_layout.add_widget(button)
 
         return app_bar_layout
