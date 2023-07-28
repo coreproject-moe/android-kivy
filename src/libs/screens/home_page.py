318797ad364eb4cb42da53813a64e21733ca73f3
Tokito <tokitousan2005@gmail.com>
2023-07-28 09:29:46 +0530

    refactor: move app_bar and rewrite in `RelativeLayout` (#20)
    * move app_bar
    
    * add appbar inside main file
    
    * add return type
    
    * change to `RelativeLayout`
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix: layout iteration issue
    
    * fix
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update home_page.py
    
    * Update main.py
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>

--- src/libs/screens/home_page.py
+++ src/libs/screens/home_page.py
@@ -1,20 +1,69 @@
+from kivy.utils import get_color_from_hex
 from kivymd.uix.screen import MDScreen
+from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.relativelayout import MDRelativeLayout
-from kivymd.uix.button import MDFillRoundFlatButton
+from kivymd.uix.button import MDFillRoundFlatButton, MDFillRoundFlatIconButton
 
 
 class HomePage(MDScreen):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
-        self.add_widget(self.layout())
+        self.md_bg_color = get_color_from_hex("#03020c")
+        self.add_widget(self.app_bar())
 
-    def layout(self):
+    # AppBar layout
+    def app_bar(self) -> MDRelativeLayout:
+        # Appbar elements
         app_bar_layout = MDRelativeLayout(
-            size_hint_y=None, height="50dp", pos_hint={"top": 1}
+            size_hint_y=0.1,
+            pos_hint={
+                "top": 1,
+            },
         )
-
-        button = MDFillRoundFlatButton(text="Logo", pos_hint={"x": 0, "top": 1})
-        app_bar_layout.add_widget(button)
+        # CoreProject logo
+        core_logo = MDFillRoundFlatButton(
+            text="Logo",
+            pos_hint={
+                "center_y": 0.5,
+                "x": 0.035,
+            },
+        )
+        # Right side elements
+        appbar_right = MDBoxLayout(
+            orientation="horizontal",
+            spacing="5dp",
+            pos_hint={"right": 0.96},
+            size_hint_x=None,
+            width="228dp",
+        )
+        # Search button
+        search_button = MDFillRoundFlatIconButton(
+            text="Search",
+            pos_hint={
+                "center_y": 0.5,
+            },
+        )
+        # Notification button
+        notification_button = MDFillRoundFlatButton(
+            text="Noti",
+            pos_hint={
+                "center_y": 0.5,
+            },
+        )
+        # Profile button
+        profile_button = MDFillRoundFlatButton(
+            text="Tokito",
+            pos_hint={
+                "center_y": 0.5,
+            },
+        )
+        # Add widgets to the main Appbar layout
+        app_bar_layout.add_widget(core_logo)
+        app_bar_layout.add_widget(appbar_right)
+        # Add other widgets
+        appbar_right.add_widget(search_button)
+        appbar_right.add_widget(notification_button)
+        appbar_right.add_widget(profile_button)
 
         return app_bar_layout
