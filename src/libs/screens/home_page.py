1b93364f9b567161b824155c065c01053c4fec0a
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-07-28 07:05:22 +0600

    Update home_page.py

--- src/libs/screens/home_page.py
+++ src/libs/screens/home_page.py
@@ -6,7 +6,7 @@ from kivymd.uix.button import MDFillRoundFlatButton, MDFillRoundFlatIconButton
 
 
 class HomePage(MDScreen):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
         self.md_bg_color = get_color_from_hex("#03020c")
@@ -17,11 +17,17 @@ class HomePage(MDScreen):
         # Appbar elements
         app_bar_layout = MDRelativeLayout(
             size_hint_y=0.1,
-            pos_hint={"top": 1},
+            pos_hint={
+                "top": 1,
+            },
         )
         # CoreProject logo
         core_logo = MDFillRoundFlatButton(
-            text="Logo", pos_hint={"center_y": 0.5, "x": 0.035}
+            text="Logo",
+            pos_hint={
+                "center_y": 0.5,
+                "x": 0.035,
+            },
         )
         # Right side elements
         appbar_right = MDBoxLayout(
@@ -32,11 +38,26 @@ class HomePage(MDScreen):
             width="228dp",
         )
         # Search button
-        search_button = MDFillRoundFlatIconButton(text="Search", pos_hint={"center_y": 0.5})
+        search_button = MDFillRoundFlatIconButton(
+            text="Search",
+            pos_hint={
+                "center_y": 0.5,
+            },
+        )
         # Notification button
-        notification_button = MDFillRoundFlatButton(text="Noti", pos_hint={"center_y": 0.5})
+        notification_button = MDFillRoundFlatButton(
+            text="Noti",
+            pos_hint={
+                "center_y": 0.5,
+            },
+        )
         # Profile button
-        profile_button = MDFillRoundFlatButton(text="Tokito", pos_hint={"center_y": 0.5})
+        profile_button = MDFillRoundFlatButton(
+            text="Tokito",
+            pos_hint={
+                "center_y": 0.5,
+            },
+        )
         # Add widgets to the main Appbar layout
         app_bar_layout.add_widget(core_logo)
         app_bar_layout.add_widget(appbar_right)
