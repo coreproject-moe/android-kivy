fdc8e6c7eda0001101f73619a4cf6439f09ac537
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-07-28 11:24:47 +0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,11 +1,13 @@
 from kivy import Config
+
+# import screens
+from kivymd.tools.hotreload.app import MDApp
+from libs.screens.home_screen import HomeScreen
+
 # config window size
 Config.set("graphics", "width", 360)
 Config.set("graphics", "height", 640)
 
-from kivymd.tools.hotreload.app import MDApp
-# import screens
-from libs.screens.home_screen import HomeScreen
 
 class MainApp(MDApp):
     def __init__(self, **kwargs):
@@ -13,6 +15,7 @@ class MainApp(MDApp):
 
     def build_app(self):
         return HomeScreen()
+
 
 if __name__ == "__main__":
     MainApp().run()
