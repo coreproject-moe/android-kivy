14ff643688034d732eca3fae446d42ba5edf7cc9
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-07-29 09:47:26 +0600

    add

--- tests/test_screen_size.py
+++ tests/test_screen_size.py
@@ -2,5 +2,11 @@ from kivy.tests.common import GraphicUnitTest
 from main import MainApp
 
 
-def test():
-    assert 1 == 1
+class TestApp(GraphicUnitTest):
+    def test_screen_size(self):
+        app = MainApp()
+        app.run()
+
+        # Assert that our configs are okay
+        self.assertEqual(app.root.width, 360)
+        self.assertEqual(app.root.height, 640)
