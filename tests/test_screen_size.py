25117e36488d939e4d448a29e4585b343defa3e5
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-07-29 08:54:04 +0600

    add pytest

--- tests/test_screen_size.py
+++ tests/test_screen_size.py
@@ -5,21 +5,10 @@ from main import MainApp
 
 
 class TestApp(GraphicUnitTest):
-    def setUp(self):
-        # Set the desired window size for testing
-        Config.set("graphics", "width", 360)
-        Config.set("graphics", "height", 640)
-        super(TestApp, self).setUp()
-
     def test_screen_size(self):
         app = MainApp()
         app.run()
 
-        # Assert that the app's window size matches the configured size
-        self.assertEqual(app.window.size, (360, 640))
-
-        # You can also check other elements on the screen, if needed
-        # For example:
-        home_screen = app.root
-        self.assertEqual(home_screen.width, 360)
-        self.assertEqual(home_screen.height, 640)
+        # Assert that our configs are okay
+        self.assertEqual(app.root.width, 360)
+        self.assertEqual(app.root.height, 640)
