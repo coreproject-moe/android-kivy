1d94dd58ac27a3de5d2223780a7942df5e045a9b
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-07-29 10:04:29 +0600

    Fix screen (#27)
    * add
    
    * Update main.py
    
    * add
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * add pytest
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update pytest.yml
    
    * Update pytest.yml
    
    * Update pytest.yml
    
    * add
    
    * Update pytest.yml
    
    * add
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update pytest.yml
    
    * Update test_screen_size.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * add
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update test_screen_size.py
    
    * Delete pytest.yml
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ tests/test_screen_size.py
@@ -0,0 +1,12 @@
+from kivy.tests.common import GraphicUnitTest
+from main import MainApp
+
+
+class TestApp(GraphicUnitTest):
+    def test_screen_size(self):
+        app = MainApp()
+        app.run()
+
+        # Assert that our configs are okay
+        self.assertEqual(app.root.width, 360)
+        self.assertEqual(app.root.height, 640)
