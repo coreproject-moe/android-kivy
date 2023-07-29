0f14f426cc002d8c6bd068db9ce7b9656075a2d0
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-07-29 17:59:14 +0600

    add

--- libs/screens/home_screen.py
+++ libs/screens/home_screen.py
@@ -1,8 +1,10 @@
 from kivy.utils import get_color_from_hex
 from kivymd.uix.screen import MDScreen
+from kivy.uix.carousel import Carousel
+from kivy.uix.image import AsyncImage
 
 # import components
-from libs.components.appbar import appbar
+from libs.components.shared.appbar import appbar
 
 
 class HomeScreen(MDScreen):
@@ -11,3 +13,10 @@ class HomeScreen(MDScreen):
 
         self.md_bg_color = get_color_from_hex("#03020c")
         self.add_widget(appbar())
+        carousel = Carousel(direction="right")
+        for i in [
+            "https://raw.githubusercontent.com/tokitou-san/CoreProject-V3-UI/4c44e794a89d8d0611eebb91d72fe6353f62c48f/static/images/characters/eliane/eliane.png"
+        ]:
+            image = AsyncImage(source=i, fit_mode="contain")
+            carousel.add_widget(image)
+        self.add_widget(carousel)
