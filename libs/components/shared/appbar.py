32c063fd7ed3c699d58af4dc72fe97adc22cf88f
tokitou-san <tokitousan2005@gmail.com>
2023-08-16 21:34:48 +0530

    add

--- libs/components/shared/appbar.py
+++ libs/components/shared/appbar.py
@@ -5,61 +5,60 @@ from kivymd.uix.button import (
     MDFillRoundFlatIconButton,
 )
 
+class AppBar(MDRelativeLayout):
+    def __init__(self, *args, **kwargs) -> None:
+        super(AppBar, self).__init__(*args, **kwargs)
 
-def appbar() -> MDRelativeLayout:
-    # Appbar elements
-    app_bar_layout = MDRelativeLayout(
-        size_hint_y=0.1,
-        pos_hint={
-            "top": 1,
-        },
-    )
-    # CoreProject logo
-    core_logo = MDFillRoundFlatButton(
-        text="Logo",
-        pos_hint={
-            "center_y": 0.5,
-            "x": 0.035,
-        },
-    )
+        self.size_hint_y = 0.1
+        self.pos_hint = {"top": 1}
+        # Add widgets
+        self.add_widget(self._create_logo())
+        self.add_widget(self._create_right_section())
 
-    # Buttons
-    appbar_buttons = {
-        "search_button": MDFillRoundFlatIconButton(
-            text="Search",
+    def _create_logo(self) -> MDFillRoundFlatButton:
+        # CoreProject logo
+        return MDFillRoundFlatButton(
+            text="Logo",
             pos_hint={
                 "center_y": 0.5,
+                "x": 0.035,
             },
-        ),
-        "notification_button": MDFillRoundFlatButton(
-            text="Noti",
-            pos_hint={
-                "center_y": 0.5,
-            },
-        ),
-        "profile_button": MDFillRoundFlatButton(
-            text="Tokito",
-            pos_hint={
-                "center_y": 0.5,
-            },
-        ),
-    }
-    # Right side elements
-    appbar_right = MDBoxLayout(
-        orientation="horizontal",
-        spacing="5dp",
-        pos_hint={
-            "right": 0.96,
-        },
-        size_hint_x=None,
-        width="228dp",
-    )
-    # Add widgets to `appbar_right`
-    for button in appbar_buttons.values():
-        appbar_right.add_widget(button)
+        )
 
-    # Add widgets to the main Appbar layout
-    app_bar_layout.add_widget(core_logo)
-    app_bar_layout.add_widget(appbar_right)
+    def _create_right_section(self) -> MDBoxLayout:
+        # Buttons
+        appbar_buttons = {
+            "search_button": MDFillRoundFlatIconButton(
+                text="Search",
+                pos_hint={
+                    "center_y": 0.5,
+                },
+            ),
+            "notification_button": MDFillRoundFlatButton(
+                text="Noti",
+                pos_hint={
+                    "center_y": 0.5,
+                },
+            ),
+            "profile_button": MDFillRoundFlatButton(
+                text="Tokito",
+                pos_hint={
+                    "center_y": 0.5,
+                },
+            ),
+        }
+        # Right side container
+        appbar_right = MDBoxLayout(
+            orientation="horizontal",
+            spacing="5dp",
+            pos_hint={
+                "right": 0.96,
+            },
+            size_hint_x=None,
+            width="228dp",
+        )
+        # Add widgets to `appbar_right`
+        for button in appbar_buttons.values():
+            appbar_right.add_widget(button)
 
-    return app_bar_layout
+        return appbar_right
