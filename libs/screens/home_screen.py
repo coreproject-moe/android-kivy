6859ab333510108baae0f876c7caeb7f8b610a0f
Tokito <tokitousan2005@gmail.com>
2023-09-08 05:44:12 +0530

    fix: Appbar layout (#51)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- libs/screens/home_screen.py
+++ libs/screens/home_screen.py
@@ -1,7 +1,5 @@
 from kivy.utils import get_color_from_hex
 from kivymd.uix.screen import MDScreen
-from kivy.uix.carousel import Carousel
-from kivy.uix.image import AsyncImage
 
 # import components
 from libs.components.shared.appbar import AppBar
@@ -13,17 +11,3 @@ class HomeScreen(MDScreen):
 
         self.md_bg_color = get_color_from_hex("#03020c")
         self.add_widget(AppBar())
-
-        carousel = Carousel(
-            direction="right",
-            loop=True,
-        )
-        for slide in [
-            "https://raw.githubusercontent.com/tokitou-san/CoreProject-V3-UI/4c44e794a89d8d0611eebb91d72fe6353f62c48f/static/images/characters/eliane/eliane.png",
-            "https://raw.githubusercontent.com/tokitou-san/CoreProject-V3-UI/4c44e794a89d8d0611eebb91d72fe6353f62c48f/static/images/characters/eliane/eliane_2.png",
-            "https://raw.githubusercontent.com/tokitou-san/CoreProject-V3-UI/4c44e794a89d8d0611eebb91d72fe6353f62c48f/static/images/characters/exy/exy.png",
-            "https://raw.githubusercontent.com/tokitou-san/CoreProject-V3-UI/4c44e794a89d8d0611eebb91d72fe6353f62c48f/static/images/characters/futaba/futaba.png",
-        ]:
-            image = AsyncImage(source=slide, fit_mode="contain")
-            carousel.add_widget(image)
-        self.add_widget(carousel)
